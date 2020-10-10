ba2e62a4889b9a116a66e95a0cb07ce7d501411f
KAHLAOUI Mathieu <mathieu.kahlaoui.pro@gmail.com>
2020-10-10 21:14:46 +0200

    Support using webpack css modules with jest and identify-obj-proxy

--- src/shared/__tests__/auto-scaling-text.js
+++ src/shared/__tests__/auto-scaling-text.js
@@ -3,6 +3,7 @@ import {render} from '@testing-library/react'
 import AutoScalingText from '../auto-scaling-text'
 
 test('renders ', () => {
-    render(<AutoScalingText/>)
+   const {debug} =  render(<AutoScalingText/>)
+   console.log(debug())
 })
 
