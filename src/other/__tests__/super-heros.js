c58cc5c52cd26c5763fc4eb65cf789f948a3d5d3
KAHLAOUI Mathieu <mathieu.kahlaoui.pro@gmail.com>
2020-10-10 22:06:25 +0200

    Generate a serializable value with jest Snapshots

---
+++ src/other/__tests__/super-heros.js
@@ -0,0 +1,30 @@
+import {getflyingSuperHero} from '../super-hero'
+
+test('return super hero that can fly', () => {
+  const hero = getflyingSuperHero()
+  expect(hero).toMatchInlineSnapshot(`
+    Array [
+      Object {
+        "name": "baba",
+        "powers": Array [
+          "tum",
+          "fly",
+        ],
+      },
+      Object {
+        "name": "crakcrak",
+        "powers": Array [
+          "boom",
+          "fly",
+        ],
+      },
+      Object {
+        "name": "crakcrak",
+        "powers": Array [
+          "boom",
+          "fly",
+        ],
+      },
+    ]
+  `)
+})
