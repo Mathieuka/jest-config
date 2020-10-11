2b8724b289b082a1c5a2cce5ff8026cb636324c2
KAHLAOUI Mathieu <mathieu.kahlaoui.pro@gmail.com>
2020-10-11 14:44:04 +0200

    Configure jest to run setup for all tests with jest setupFileAfterEnv

--- src/__tests__/calculator.js
+++ src/__tests__/calculator.js
@@ -1,7 +1,18 @@
 import React from 'react'
-import {render} from '@testing-library/react'
+import {render, fireEvent} from '@testing-library/react'
 import Calculator from '../calculator'
 
-test('Should render calculator component', () => {
-  render(<Calculator />)
+/*
+ `toHaveTextContent` assertion is imported from `@testing-library/jest-dom/extend-expect` see jest.config.js
+*/
+
+test('The cleat button switches fro AC to C when there is an entry', () => {
+  const {getByText} = render(<Calculator />)
+  const clearButton = getByText('AC')
+
+  fireEvent.click(getByText(/3/))
+  expect(clearButton).toHaveTextContent('C')
+
+  fireEvent.click(clearButton)
+  expect(clearButton).toHaveTextContent('AC')
 })
