96fa2b046afba872572909864ca79e043f92ce77
KAHLAOUI Mathieu <mathieu.kahlaoui.pro@gmail.com>
2020-10-19 19:57:35 +0200

    Configure jest to report code coverage on project file

--- jest.config.js
+++ jest.config.js
@@ -6,5 +6,14 @@ module.exports = {
     '\\.css$': require.resolve('./test/style-mock.js'),
   },
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
-  snapshotSerializers: ['jest-emotion']
+  snapshotSerializers: ['jest-emotion'],
+  collectCoverageFrom: ['**/src/**/*.js'],
+  coverageThreshold: {
+    global: {
+      statement: 34,
+      branches: 22,
+      function: 23,
+      lines: 22
+    }
+  }
 }
